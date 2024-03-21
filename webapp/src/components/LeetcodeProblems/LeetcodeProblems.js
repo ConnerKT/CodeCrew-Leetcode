d8ceaaa09ff6bcfcef30c7e95a754cb7eb581061
ConnerKT <connerthompson121@gmail.com>
2024-03-21 14:23:36 -0500

    added dummy data

--- webapp/src/components/LeetcodeProblems/LeetcodeProblems.js
+++ webapp/src/components/LeetcodeProblems/LeetcodeProblems.js
@@ -1,44 +1,52 @@
-import React from 'react'
-import '../LeetcodeProblems/LeetcodeProblems.css'; 
+import React, { useState } from "react";
+import "../LeetcodeProblems/LeetcodeProblems.css";
+import Data from '../../leetcode_challenges.json'
 
 function LeetcodeProblems() {
+  const [selectedProblem, setSelectedProblem] = useState(null);
+
+  const handleSelectProblem = (event) => {
+    const problemId = parseInt(event.target.value);
+    const problem = Data.find(problem => problem.id === problemId);
+    setSelectedProblem(problem);
+  };
+
   return (
-    <div id="problemContainer" class="container">
-    <div id="problemName" class="title">
-      <h1>Palindrome Number</h1>
+    <div id="problemContainer" className="container">
+      <select onChange={handleSelectProblem} value={selectedProblem ? selectedProblem.id : ""}>
+        <option value="">Select a problem</option>
+        {Data.map((problem) => (
+          <option key={problem.id} value={problem.id}>
+            {problem.title}
+          </option>
+        ))}
+      </select>
+      {selectedProblem && (
+        <>
+          <div id="problemName" className="title">
+            <h1>{selectedProblem.title}</h1>
+          </div>
+          <div className="box-container">
+            <div id="problemDescription" className="content">
+              {selectedProblem.description}
+            </div>
+            {selectedProblem.examples && (
+              <div id="problemExample" className="content">
+                {selectedProblem.examples.map((example, index) => (
+                  <div key={index}>
+                    <strong>Example {index + 1}:</strong> Input: {example.input} Output: {example.output}
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
+          <a href={`https://leetcode.com/problems/${selectedProblem.title.toLowerCase().replace(/\s+/g, "-")}/`}>
+            Problem Link
+          </a>
+        </>
+      )}
     </div>
-    <div class="box-container">
-      <div id="problemDescription" class="content">
-      Given an integer x, return true if x is a
-palindrome
-, and false otherwise.
-      </div>
-      <div id="problemExample" class="content">
-      Example 1:
-
-Input: x = 121
-Output: true
-Explanation: 121 reads as 121 from left to right and from right to left.
-<br></br>
-Example 2:
-
-Input: x = -121
-Output: false
-Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
-<br></br>
-Example 3:
-
-Input: x = 10
-Output: false
-Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
-
- 
-      </div>
-    </div>
-  </div>
-  
-
-  )
+  );
 }
 
-export default LeetcodeProblems
+export default LeetcodeProblems;
